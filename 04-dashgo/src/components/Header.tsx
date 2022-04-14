d22f59775ca8bba6dc7ce1f6df36ac52be6a8a5c
Andrey Araujo <jaab.andrey@gmail.com>
2022-04-14 09:53:09 -0300

    criando menu lateral

--- 04-dashgo/src/components/Header.tsx
+++ 04-dashgo/src/components/Header.tsx
@@ -1,6 +1,6 @@
-import { RiSearchLine } from 'react-icons/ri';
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri';
 
-import { Flex, Icon, Input, Text } from '@chakra-ui/react';
+import { Avatar, Box, Flex, HStack, Icon, Input, Text } from '@chakra-ui/react';
 
 export function Header() {
   return (
@@ -43,6 +43,36 @@ export function Header() {
           _placeholder={{ color: 'gray.400' }}
         />
         <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Andrey Araujo</Text>
+            <Text color="gray.300" fontSize="small">
+              contato@andreyaraujo.dev
+            </Text>
+          </Box>
+
+          <Avatar
+            size="md"
+            name="Andrey Araujo"
+            src="https://github.com/andreyaraujo-dev.png"
+          />
+        </Flex>
       </Flex>
     </Flex>
   );
