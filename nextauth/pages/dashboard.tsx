918f6eb9e961cdf4a96d0b5e42aeaafa80e79998
Andrey Araujo <jaab.andrey@gmail.com>
2022-05-20 15:25:27 -0300

    adicionando verificacao de autenticacao no SSR

--- nextauth/pages/dashboard.tsx
+++ nextauth/pages/dashboard.tsx
@@ -1,6 +1,8 @@
 import { useContext, useEffect } from 'react'
 import { AuthContext } from '../contexts/AuthContext'
-import { api } from '../services/api';
+import { api } from '../services/apiClient';
+import { withSSRAuth } from '../utils/withSSRAuth';
+import { setupAPIClient } from '../services/api';
 
 export default function Dashboard() {
   useEffect(() => {
@@ -10,3 +12,13 @@ export default function Dashboard() {
   const { user } = useContext(AuthContext);
   return <h1>Hello {user?.email}</h1>
 }
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+  const apiClient = setupAPIClient(ctx);
+  const response = await apiClient.get('/me');
+  console.log(response.data)
+
+  return {
+    props: {}
+  }
+})
