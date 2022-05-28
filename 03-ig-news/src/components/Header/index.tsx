9012fcd6d5e40ef73970d22dfdc78bda48fb4218
Jacksson Andrey <jaab.andrey@gmail.com>
2022-05-28 10:55:02 -0300

    adicionando testes no header

--- 03-ig-news/src/components/Header/index.tsx
+++ 03-ig-news/src/components/Header/index.tsx
@@ -1,14 +1,13 @@
 import Image from 'next/image';
 import { ActiveLink } from '../ActiveLink';
 import { SignInButton } from '../SingInButton';
-import logoImg from '../../../public/images/logo.svg';
 import styles from './styles.module.scss';
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <Image src={logoImg} alt="ig.news" />
+        <Image src="/images/logo.svg" alt="ig.news" width={100} height={90} />
         <nav>
           <ActiveLink activeClassName={styles.active} href='/'>
             <a>Home</a>
